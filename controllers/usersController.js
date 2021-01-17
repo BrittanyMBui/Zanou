985f914194ac50b1e48636cbbdf4adc8a265e437
brittanymbui <brittymbui@gmail.com>
2021-01-17 11:32:20 -0800

    Created sign up page and log in page with paths in controller

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -5,4 +5,12 @@ router.get('/', (req,res)=>{
     res.send('users route');
 });
 
+router.get('/new', (req, res)=>{
+    res.render('users/newUser');
+});
+
+router.get('/login', (req, res)=>{
+    res.render('users/loginUser');
+})
+
 module.exports = router;
