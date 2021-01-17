a47c7a2ac33e68677017d1e710805747181eb4f8
brittanymbui <brittymbui@gmail.com>
2021-01-17 11:23:42 -0800

    Changed mongo path

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const connectionString = process.env.MONGODB_URI || 'mongodb://localhost:27017/journal';
+const connectionString = process.env.MONGO_URI || 'mongodb://localhost:27017/journal';
 
 mongoose.connect(connectionString, {
     useNewUrlParser: true,
